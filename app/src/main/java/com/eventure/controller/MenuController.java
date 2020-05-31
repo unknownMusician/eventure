5d4f46b49c35f8060d4605195a4e437d1b934b37
unknownMusician <serega3assassin2002@gmail.com>
2020-05-31 18:32:50 +0300

    EventList Controller

--- app/src/main/java/com/eventure/controller/MenuController.java
+++ app/src/main/java/com/eventure/controller/MenuController.java
@@ -1,9 +1,18 @@
 package com.eventure.controller;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MenuController extends FrontController {
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    protected MenuController(){
+        super();
+    }
 
     public ArrayList<String> getButtonList(){
         return new ArrayList<String>(Arrays.asList("Event list", "Calender", "Map", "Settings"));
