0fc1d4c73ec7c85e8672de270c60027b6bc7e5ed
unknownMusician <serega3assassin2002@gmail.com>
2020-05-31 22:33:34 +0300

    Update EventListController.java

--- app/src/main/java/com/eventure/controller/EventListController.java
+++ app/src/main/java/com/eventure/controller/EventListController.java
@@ -13,23 +13,6 @@ import java.util.ArrayList;
 
 public class EventListController extends FrontController {
 
-    public static EventListController instance;
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public static EventListController getInstance() {
-        if (instance == null) {
-            return new EventListController();
-        }
-        return instance;
-    }
-
-    ////////// static line //////////
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    protected EventListController(){ super(); }
-
-    ///// ///// controller ///// /////
-
     @RequiresApi(api = Build.VERSION_CODES.N)
     public ArrayList<String> getEventTitleList(){
         return ServiceFactory.getInstance().getEventService().getEventTitleList();
